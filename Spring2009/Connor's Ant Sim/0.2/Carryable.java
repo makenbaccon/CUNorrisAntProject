e6641713a3c67d24638ed71b6cbfc29687ce7ed5
unknown <makenbaccon@gmail.com>
2012-12-07 16:02:39 -0700

    changed comments in foragers.nls

--- Spring2009/Connor's Ant Sim/0.2/Carryable.java
+++
@@ -1,5 +0,0 @@
-public interface Carryable {
-    public void carry(double x, double y);
-    public boolean carried();
-    public void setCarry(boolean state);
-}
--- Spring2009/Connor's Ant Sim/0.2/Thing.java
+++
@@ -1,9 +0,0 @@
-public abstract class Thing {
-    double x, y;
-
-    public double getX() { return x; }
-    public double getY() { return y; }
-    public void collided(){
-    	
-    }
-}
